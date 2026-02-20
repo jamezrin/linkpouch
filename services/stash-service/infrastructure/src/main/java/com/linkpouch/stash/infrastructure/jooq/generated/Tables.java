91e20a8badbe91e23840c5d25131bdc6237067d5
Jaime Martínez Rincón <mrjaime1999@gmail.com>
2026-02-20 01:23:45 +0100

    chore(jooq): remove fake generated classes, configure jOOQ codegen to run on demand

--- services/stash-service/infrastructure/src/main/java/com/linkpouch/stash/infrastructure/jooq/generated/Tables.java
+++
@@ -1,7 +0,0 @@
-package com.linkpouch.stash.infrastructure.jooq.generated;
-
-import com.linkpouch.stash.infrastructure.jooq.generated.tables.Links;
-
-public class Tables {
-    public static final Links LINKS = Links.LINKS;
-}
